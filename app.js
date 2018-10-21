7b1e41909dbcdbde0af2a8402775dea1f7bca356
aravindraj <aravinu19@gmail.com>
2018-10-21 11:58:40 +0530

    added transcript id fix

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ var transcriber = function(app){
       }
       else{
         console.log(data.Body.toString());
-        res.json(JSON.parse(data.Body.toString()));
+        res.json(JSON.parse(data.Body.results.transcripts[0].transcript.toString()));
       }
 
     })
