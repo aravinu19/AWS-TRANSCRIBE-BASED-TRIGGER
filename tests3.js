d7fc214e0155f13d9d6387f666331b5668ff96ac
aravindraj <aravinu19@gmail.com>
2018-10-21 20:24:04 +0530

    ok

--- tests3.js
+++ tests3.js
@@ -6,6 +6,10 @@ var paras = {
     Bucket: "cloudvoicepath"
 }
 
+var data_json = "{";
+
+var files_in_a_day = new Array(30).fill(0);
+
 s3.listObjects(paras, (err, data) => {
     if(err) console.log(err);
     else{
@@ -15,11 +19,41 @@ s3.listObjects(paras, (err, data) => {
         var count = 0;
 
         list.forEach(element => {
-            console.log(element.Key);
-            count++;
+            if(element.Key.split(".")[1] == "mp3" && element.Key.split(".")[2] != "json"){
+                console.log(element.Key + "     " + element.LastModified.toString().split(" "));
+                count++;
+
+                files_in_a_day[element.LastModified.getDate()]++;// = files_in_a_day[element.LastModified.toString().split(" ")[2]] + 1;
+
+            }
         });
 
         console.log(`\n\nTotal Files Count ${count}`);
 
+        for(var index = 0; index < 30; index++){
+            
+            if(files_in_a_day[index] > 0){
+                console.log(files_in_a_day[index]);
+            }
+
+            data_json += create_data(index+1, files_in_a_day[index], "OCTOBER");
+            
+        }
+
+        data_json += "}";
+
+        console.log(data_json);
+
     }
 })
+
+function create_data(date, files_count, month){
+    return `{
+        "Dag":"vrijdag",
+        "Datum":"${date} ${month}.",
+        "Slaapeffi":0.85,
+        "Slaapuren":${files_count},
+        "Beginslapen":"01:52",
+        "Eindslapen":"08:58",
+    },`
+}
