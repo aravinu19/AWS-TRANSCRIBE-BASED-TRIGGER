a91c6b7241a6a704b383866e6bc88bb2d41a93c3
aravindraj <aravinu19@gmail.com>
2018-11-02 07:56:23 +0530

    Added Features to D3 Graph API

--- s3_object_history_generator.js
+++ s3_object_history_generator.js
@@ -38,7 +38,7 @@ var object_history_generator = function(required_month, start_date, end_date, ca
 
             for(var index = start_date; index <= end_date; index++){
 
-                data_json += create_data(index, files_in_days[index], "OCT"); //Month needs method to change according to request month data
+                data_json += prepare_data_for_d3_graph(index, files_in_days[index], required_month); //Month needs method to change according to request month data
 
             }
 
@@ -53,15 +53,65 @@ var object_history_generator = function(required_month, start_date, end_date, ca
 };
 
 
-function create_data(date, files_count, month){
-    return `{
-        "Dag":"vrijdag",
-        "Datum":"${date} ${month}.",
-        "Slaapeffi":0.85,
-        "Slaapuren":${files_count},
-        "Beginslapen":"01:52",
-        "Eindslapen":"08:58",
-    },`
+function create_data(date, files_count, month, bar_color){
+    if (files_count > 0) {
+        return `{
+            "Dag":"vrijdag",
+            "Datum":"${date} ${month}.",
+            "Slaapeffi": ${bar_color},
+            "Slaapuren":${files_count},
+            "Beginslapen":"${files_count} Files"
+        },`
+    }else{
+        return `{
+            "Dag":"vrijdag",
+            "Datum":"${date} ${month}.",
+            "Slaapeffi":0.85,
+            "Slaapuren":${files_count}
+        },`
+    }
+}
+
+function prepare_data_for_d3_graph(date, files_count, month){
+
+    var requested_month = get_selected_month_string(month);
+
+    var bar_color = get_color_based_on_files_count(files_count);
+
+    return create_data(date, files_count, requested_month, bar_color);
+
+}
+
+function get_selected_month_string(month){
+
+    switch(month){
+        case 0: return "JAN"; 
+        case 1: return "FEB"; 
+        case 2: return "MAR";
+        case 3: return "APR";
+        case 4: return "MAY";
+        case 5: return "JUN";
+        case 6: return "JUL";
+        case 7: return "AUG";
+        case 8: return "SEP";
+        case 9: return "OCT";
+        case 10: return "NOV";
+        case 11: return "DEC";
+        default: return "OCT";
+    }
+
+}
+
+function get_color_based_on_files_count(files_count){
+
+    if (files_count <= 5) {
+        return (0.1);
+    }else if(files_count <= 10){
+        return (1);
+    }else{
+        return (0.85);
+    }
+
 }
 
 module.exports = object_history_generator;
